1e80f3e80409a8f81ad8747e892f5995db2310f2
Korver <korver2017@gmail.com>
2018-11-12 06:06:10 +0800

    set intro animation

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
   $('body').addClass('bodyShow');
   $('nav').addClass('navShow');
+  if ($(window).scrollTop() < $('#about').outerHeight() / 4)
+    setTimeout(function(){
+      $('h1').addClass('animated jackInTheBox');
+    }, 1500);  
+  
 
+  $(window).on('scroll', function(){
+    if($(window).scrollTop() < $('#about').outerHeight() / 4)
+      $('h1').addClass('animated jackInTheBox');
+
+    if ($(window).scrollTop() > ($('#skills').offset().top) / 3 * 2)
+      $('.dev-icons').css('opacity', 1).addClass('animated bounceInRight');
+  });
 });
