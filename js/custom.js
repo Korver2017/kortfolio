fbf79e5bc7adbbb1bfa37c8679aee5a503fbe990
Korver <korver2017@gmail.com>
2018-11-13 09:59:01 +0800

    build up introduction part, add content and animation

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 
   $('body').addClass('show');
   $('nav').addClass('navShow');
+  // $('#introduction .subheadingGroup').hide();
   if ($window.scrollTop() < $('#cover').outerHeight() / 4) {
     setTimeout(function () {
       $('h1').addClass('show animated jackInTheBox show');
@@ -19,12 +20,34 @@ $(document).ready(function () {
       // console.log($windowScrollTop);
       $('.scrollMoveOut').stop(true).animate({}, 1000, function () {
         $(this).css({
-          'transform': 'translateX(' + ($windowScrollTop / 3) + 'px)'
+          'transform': 'translateX(' + ($windowScrollTop / 20) + 'px)'
         })
       });
 
       if ($windowScrollTop > ($('#skills').offset().top) / 3 * 2) {
         $('.dev-icons').css('opacity', 1).addClass('animated bounceInRight');
+      }
+
+      if ($windowScrollTop > ($('#introduction').offset().top) / 3 * 2) {
+        // $('#introduction .subheading').css('opacity', 1).addClass('animated fadeInRight');
+        // $('#introduction .subheadingGroup:first-child').delay(1000).addClass('animated fadeInRight');
+        // $('#introduction .subheadingGroup:nth-child(2)').delay(3000).addClass('animated fadeInRight');
+        
+        setTimeout(function() {
+          $('.subheadingGroup1').css('opacity', 1).addClass('animated fadeInRight');
+        }, 500);
+        setTimeout(function () {
+          $('.subheadingGroup2').css('opacity', 1).addClass('animated fadeInRight');
+        }, 700);
+        setTimeout(function () {
+          $('.subheadingGroup3').css('opacity', 1).addClass('animated fadeInRight');
+        }, 900);
+        setTimeout(function () {
+          $('.subheadingGroup4').css('opacity', 1).addClass('animated fadeInRight');
+        }, 1100);
+        setTimeout(function () {
+          $('.subheadingGroup5').css('opacity', 1).addClass('animated fadeInRight');
+        }, 1300);
       }
     }
 
@@ -34,5 +57,7 @@ $(document).ready(function () {
         'transform': 'translateY(' + ($moveUnit / 5) + 'vh)'
       });
     });
+
+    
   });
 });
