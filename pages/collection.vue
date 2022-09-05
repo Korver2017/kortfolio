6d036eca1cdbd5615beaf3fb41ab38aea2055c92
Korver <korver2017@gmail.com>
2022-09-05 14:27:55 +0800

    feat: Update works collection

--- pages/collection.vue
+++ pages/collection.vue
@@ -9,90 +9,54 @@
     >
       <div class="my-auto">
         <h2 class="text-primary mb-5">Works Collection</h2>
-
         <div class="cardWrap d-flex flex-row">
           <div class="card col-lg-3 col-12">
             <img
               class="card-img-top mt-2"
-              height="139"
-              src="@/assets/images/countdown.png"
+              src="@/assets/images/konstructor.png"
               alt="Card image cap"
             />
-            <div class="card-body">
-              <h4 class="card-title">Vue.js Countdown Component</h4>
-              <h5>By Wrapping Up jQuery Plugin</h5>
-
-              <br />
+            <div class="card-body pb-0">
+              <h4 class="card-title">Konstructor</h4>
               <p class="card-text">
-                Vue.js<br />
-                Component Management<br />
-                Props & Custom Event<br />
-                UI Layout
+                Vue 3 / Pinia / Vue Router<br />
+                Vite / Bootstrap 5<br />
               </p>
             </div>
-            <div class="card-body">
+            <div class="card-body pt-3 pb-4">
               <a
-                href="https://korver2017.github.io/vuejs-countdown-component/"
-                class="btn btn-outline-primary mb-2"
+                href="https://github.com/Korver2017/konstructor"
+                class="btn btn-outline-primary mr-1"
                 target="_blank"
-                >Visit Website</a
-              ><br />
+                >GitHub</a
+              >
               <a
-                href="https://github.com/Korver2017/vuejs-countdown-component"
+                href="https://korver2017.github.io/konstructor/"
                 class="btn btn-outline-primary"
                 target="_blank"
-                >GitHub Repo</a
+                >Website</a
               >
             </div>
           </div>
-
           <div class="card col-lg-3 col-12">
             <img
               class="card-img-top mt-2"
               src="@/assets/images/portfolio.png"
               alt="Card image cap"
             />
-            <div class="card-body">
-              <h4 class="card-title">Korver Portfolio</h4>
+            <div class="card-body pb-0">
+              <h4 class="card-title">Kortfolio</h4>
               <p class="card-text">
-                Parallax Scrolling<br />
-                Animation & Interaction<br />
-                jQuery<br />
-                CSS Transition
+                Nuxt / Vue<br />
+                Bootstrap / CSS Transition<br />
               </p>
             </div>
-            <div class="card-body">
+            <div class="card-body pt-3 pb-4">
               <a
-                href="https://github.com/Korver2017/bootstrap4-portfolio"
+                href="https://github.com/Korver2017/kortfolio"
                 class="btn btn-outline-primary"
                 target="_blank"
-                >GitHub Repo</a
-              >
-            </div>
-          </div>
-
-          <div class="card col-lg-3 col-12">
-            <img
-              class="card-img-top mt-2"
-              src="@/assets/images/toilet-map.png"
-              alt="Card image cap"
-            />
-            <div class="card-body">
-              <h4 class="card-title">Hualien City Public Toilet Map</h4>
-              <p class="card-text">Google Maps API</p>
-            </div>
-            <div class="card-body">
-              <a
-                href="https://korver2017.github.io/hualien-city-public-toilet-map/"
-                class="btn btn-outline-primary mb-2"
-                target="_blank"
-                >Visit Website</a
-              ><br />
-              <a
-                href="https://github.com/Korver2017/hualien-city-public-toilet-map"
-                class="btn btn-outline-primary"
-                target="_blank"
-                >GitHub Repo</a
+                >GitHub</a
               >
             </div>
           </div>
