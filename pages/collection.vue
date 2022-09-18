f01816992fe245c41d96320235778baf2f47ef09
Korver <korver2017@gmail.com>
2022-09-18 13:54:07 +0800

    feat: Extract pages HTML structure to be shared component

--- pages/collection.vue
+++ pages/collection.vue
@@ -1,69 +1,61 @@
 <template>
-  <!-- Main content -->
-  <div class="main container-fluid ml-3 p-0">
-    <!-- Works collection -->
-    <section
-      class="resume-section p-3 p-lg-5 d-flex flex-column"
-      :class="{ display: isTriggered }"
-      id="collection"
-    >
-      <div class="my-auto mx-2">
-        <h2 class="text-primary mb-5">Works Collection</h2>
-        <div class="cardWrap d-flex flex-row">
-          <div class="card">
-            <img
-              class="card-img-top"
-              src="@/assets/images/konstructor.png"
-              alt="Card image cap"
-            />
-            <div class="card-body pb-0">
-              <h4 class="card-title">Konstructor</h4>
-              <p class="card-text">
-                Vue 3 / Pinia / Vite / Vue Router<br />
-                Bootstrap 5<br />
-              </p>
-            </div>
-            <div class="card-body pt-3 pb-4">
-              <a
-                href="https://github.com/Korver2017/konstructor"
-                class="btn btn-outline-primary mr-1"
-                target="_blank"
-                >GitHub</a
-              >
-              <a
-                href="https://korver2017.github.io/konstructor/"
-                class="btn btn-outline-primary"
-                target="_blank"
-                >Website</a
-              >
-            </div>
+  <Wrapper page="collection">
+    <div class="my-auto mx-2">
+      <h2 class="text-primary mb-5">Works Collection</h2>
+      <div class="cardWrap d-flex flex-row">
+        <div class="card">
+          <img
+            class="card-img-top"
+            src="@/assets/images/konstructor.png"
+            alt="Card image cap"
+          />
+          <div class="card-body pb-0">
+            <h4 class="card-title">Konstructor</h4>
+            <p class="card-text">
+              Vue 3 / Pinia / Vite / Vue Router<br />
+              Bootstrap 5<br />
+            </p>
           </div>
-          <div class="card">
-            <img
-              class="card-img-top"
-              src="@/assets/images/portfolio.png"
-              alt="Card image cap"
-            />
-            <div class="card-body pb-0">
-              <h4 class="card-title">Kortfolio</h4>
-              <p class="card-text">
-                Nuxt / Vue<br />
-                Bootstrap / CSS Animation<br />
-              </p>
-            </div>
-            <div class="card-body pt-3 pb-4">
-              <a
-                href="https://github.com/Korver2017/kortfolio"
-                class="btn btn-outline-primary"
-                target="_blank"
-                >GitHub</a
-              >
-            </div>
+          <div class="card-body pt-3 pb-4">
+            <a
+              href="https://github.com/Korver2017/konstructor"
+              class="btn btn-outline-primary mr-1"
+              target="_blank"
+              >GitHub</a
+            >
+            <a
+              href="https://korver2017.github.io/konstructor/"
+              class="btn btn-outline-primary"
+              target="_blank"
+              >Website</a
+            >
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card-img-top"
+            src="@/assets/images/portfolio.png"
+            alt="Card image cap"
+          />
+          <div class="card-body pb-0">
+            <h4 class="card-title">Kortfolio</h4>
+            <p class="card-text">
+              Nuxt / Vue<br />
+              Bootstrap / CSS Animation<br />
+            </p>
+          </div>
+          <div class="card-body pt-3 pb-4">
+            <a
+              href="https://github.com/Korver2017/kortfolio"
+              class="btn btn-outline-primary"
+              target="_blank"
+              >GitHub</a
+            >
           </div>
         </div>
       </div>
-    </section>
-  </div>
+    </div>
+  </Wrapper>
 </template>
 
 <script>
@@ -75,11 +67,7 @@ export default {
   data() {
     return {}
   },
-  computed: {
-    isTriggered() {
-      return this.$store.getters.isTriggered
-    },
-  },
+  computed: {},
   mounted() {},
 }
 </script>
--- pages/skills.vue
+++ pages/skills.vue
@@ -1,20 +1,12 @@
 <template>
-  <!-- Main content -->
-  <div class="main container-fluid ml-3 p-0">
-    <!-- Skills section -->
-    <section
-      class="resume-section p-3 p-lg-5 d-flex flex-column"
-      :class="{ display: isTriggered }"
-      id="skills"
-    >
-      <div class="my-auto mx-2">
-        <h2 class="text-primary mb-5">Skills</h2>
-        <div class="subheading mb-3">Programming Skills, Tools, and Others</div>
-        <!-- Skill icons -->
-        <Icons />
-      </div>
-    </section>
-  </div>
+  <Wrapper page="skills">
+    <div class="my-auto mx-2">
+      <h2 class="text-primary mb-5">Skills</h2>
+      <div class="subheading mb-3">Programming Skills, Tools, and Others</div>
+      <!-- Skill icons -->
+      <Icons />
+    </div>
+  </Wrapper>
 </template>
 
 <script>
@@ -23,11 +15,7 @@ export default {
   head: {
     title: 'Skills',
   },
-  computed: {
-    isTriggered() {
-      return this.$store.getters.isTriggered
-    },
-  },
+  computed: {},
 }
 </script>
 
