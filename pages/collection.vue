9cc52191cf1550a69c4e34c8058eb16b9ae6b7b5
Korver <korver2017@gmail.com>
2022-09-18 14:14:34 +0800

    enhance: Modify HTML structure to refine collection layout

--- pages/collection.vue
+++ pages/collection.vue
@@ -2,55 +2,61 @@
   <Wrapper page="collection">
     <div class="my-auto mx-2">
       <h2 class="text-primary mb-5">Works Collection</h2>
-      <div class="cardWrap d-flex flex-row">
-        <div class="card">
-          <img
-            class="card-img-top"
-            src="@/assets/images/konstructor.png"
-            alt="Card image cap"
-          />
-          <div class="card-body pb-0">
-            <h4 class="card-title">Konstructor</h4>
-            <p class="card-text">
-              Vue 3 / Pinia / Vite / Vue Router<br />
-              Bootstrap 5<br />
-            </p>
+      <div class="container">
+        <div class="cardWrap d-flex flex-row">
+          <div class="col-md-6 mb-4">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="@/assets/images/konstructor.png"
+                alt="Card image cap"
+              />
+              <div class="card-body pb-0">
+                <h4 class="card-title">Konstructor</h4>
+                <p class="card-text">
+                  Vue 3 / Pinia / Vite / Vue Router<br />
+                  Bootstrap 5<br />
+                </p>
+              </div>
+              <div class="card-body pt-3 pb-4">
+                <a
+                  href="https://github.com/Korver2017/konstructor"
+                  class="btn btn-outline-primary mr-1"
+                  target="_blank"
+                  >GitHub</a
+                >
+                <a
+                  href="https://korver2017.github.io/konstructor/"
+                  class="btn btn-outline-primary"
+                  target="_blank"
+                  >Website</a
+                >
+              </div>
+            </div>
           </div>
-          <div class="card-body pt-3 pb-4">
-            <a
-              href="https://github.com/Korver2017/konstructor"
-              class="btn btn-outline-primary mr-1"
-              target="_blank"
-              >GitHub</a
-            >
-            <a
-              href="https://korver2017.github.io/konstructor/"
-              class="btn btn-outline-primary"
-              target="_blank"
-              >Website</a
-            >
-          </div>
-        </div>
-        <div class="card">
-          <img
-            class="card-img-top"
-            src="@/assets/images/portfolio.png"
-            alt="Card image cap"
-          />
-          <div class="card-body pb-0">
-            <h4 class="card-title">Kortfolio</h4>
-            <p class="card-text">
-              Nuxt / Vue<br />
-              Bootstrap / CSS Animation<br />
-            </p>
-          </div>
-          <div class="card-body pt-3 pb-4">
-            <a
-              href="https://github.com/Korver2017/kortfolio"
-              class="btn btn-outline-primary"
-              target="_blank"
-              >GitHub</a
-            >
+          <div class="col-md-6 mb-4">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="@/assets/images/portfolio.png"
+                alt="Card image cap"
+              />
+              <div class="card-body pb-0">
+                <h4 class="card-title">Kortfolio</h4>
+                <p class="card-text">
+                  Nuxt / Vue<br />
+                  Bootstrap / CSS Animation<br />
+                </p>
+              </div>
+              <div class="card-body pt-3 pb-4">
+                <a
+                  href="https://github.com/Korver2017/kortfolio"
+                  class="btn btn-outline-primary"
+                  target="_blank"
+                  >GitHub</a
+                >
+              </div>
+            </div>
           </div>
         </div>
       </div>
